7dfbe5c9284e0c522c17588c89b3d501ae78d357
SL1900 <david_super@inbox.ru>
2023-06-24 13:30:47 +0900

    Actually fix manifest route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { base } from "$app/paths";
+
+    // 
+</script>
+
+<svelte:head>
+		<link rel="manifest" href="{base}/manifest.json">
+</svelte:head>
+<slot />
+
+<style>
+    /*  */
+</style>
