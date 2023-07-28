a0f1511c0364abb304129932547e30435ae43d5d
SL1900 <sonic.amy.tails.shadow.david@gmail.com>
2023-07-28 10:38:11 +0900

    Add paginator component

---
+++ src/components/Paginator.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    // 
+    export let max = 0;
+    export let current = 0;
+    export let callback: (direction: number) => void;
+
+    function PrevPage()
+    {
+        callback(-1);
+    }
+    function NextPage()
+    {
+        callback(1);
+    }
+</script>
+
+<main>
+    <!--  -->
+    <button on:click={PrevPage}>Prev</button>
+    <div class="state">{current} / {max}</div>
+    <button on:click={NextPage}>Next</button>
+</main>
+
+<style>
+    /*  */
+</style>
