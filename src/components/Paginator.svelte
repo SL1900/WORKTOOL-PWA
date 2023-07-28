d003bc9b927102e6756041f0a1756273a8149c0f
SL1900 <sonic.amy.tails.shadow.david@gmail.com>
2023-07-28 14:54:19 +0900

    Style pagination better

--- src/components/Paginator.svelte
+++ src/components/Paginator.svelte
@@ -16,11 +16,32 @@
 
 <main>
     <!--  -->
-    <button on:click={PrevPage}>Prev</button>
+    <button class="prev-btn {current == 1 ? "hidden" : ""}" on:click={PrevPage}><img src = "ArrowRight.svg" alt="Arrow Right"/></button>
     <div class="state">{current} / {max}</div>
-    <button on:click={NextPage}>Next</button>
+    <button class="next-btn {current == max ? "hidden" : ""}" on:click={NextPage}><img src = "ArrowRight.svg" alt="Arrow Right"/></button>
 </main>
 
 <style>
     /*  */
+    main{
+        display: flex;
+        align-items: center;
+        gap: 2px;
+    }
+    button{
+        width: 2rem;
+        height: 2rem;
+    }
+    .next-btn img{
+        width: 100%;
+        height: 100%;
+    }
+    .prev-btn img{
+        width: 100%;
+        height: 100%;
+        transform: rotate(180deg);
+    }
+    .hidden{
+        opacity: 0;
+    }
 </style>
