fe7150d780023b1e457545bc0f0d6744ec3d9fd1
SL1900 <43700351+SL1900@users.noreply.github.com>
2023-12-22 08:43:55 +0900

    Upload Page

---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+import {PUBLIC_DATA_URL} from "$env/static/public";
+async function change(e)
+{
+let file = e.target.files[0];
+let data = new FormData();
+data.append("file", file);
+await fetch(PUBLIC_DATA_URL + "/upload" , {
+    method: "POST", 
+    body: data 
+});
+}
+</script>
+
+<main>
+<input type="file" on:change={(e)=>change(e)}>
+</main>
