71d599e0e2198679c9003183c871b2c7ad4f5e10
SL1900 <43700351+SL1900@users.noreply.github.com>
2023-12-22 08:50:42 +0900

    Upload Indicator

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 import {PUBLIC_DATA_URL} from "$env/static/public";
+
+let uploaded = false;
+let uploading = false;
+
 async function change(e)
 {
+    uploading = true;
 let file = e.target.files[0];
 let data = new FormData();
 data.append("file", file);
@@ -9,9 +14,13 @@ await fetch(PUBLIC_DATA_URL + "/upload" , {
     method: "POST", 
     body: data 
 });
+uploading = false;
+uploaded = true;
 }
 </script>
 
 <main>
 <input type="file" on:change={(e)=>change(e)}>
+{#if uploading}UPLOADING. . .  {/if}
+{#if uploaded}Uploaded! ! ! {/if}
 </main>
